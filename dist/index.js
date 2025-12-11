6e9bcd63d5c9495b5f03b3a28b373cbcbf1c3f9f
Imtiaz Ali <191659116+Imtiaz-Ali17314@users.noreply.github.com>
2025-12-11 12:17:09 +0500

    initialize TypeScript project with basic setup and configuration

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return "Hello, " + name + "! Welcome to new typescript journey.";
+}
+const name = "Imtiaz Ali";
+console.log(greet(name));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function greet(name: string): string {
+  return "Hello, " + name + "! Welcome to new typescript journey.";
+}
+
+const name: string = "Imtiaz Ali";
+
+console.log(greet(name));
