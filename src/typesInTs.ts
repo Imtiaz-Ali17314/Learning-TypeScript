6b9fce8486260d5ba3de3676d83faa52d8148b1c
Imtiaz Ali <191659116+Imtiaz-Ali17314@users.noreply.github.com>
2025-12-13 11:03:42 +0500

    add inference and annotation examples in TypeScript

---
+++ src/typesInTs.ts
@@ -0,0 +1,11 @@
+//  Inference and Annotation
+
+// Inference : Apny app data type samajhna.
+let name = "Imtiaz Ali";
+let rollNo = 22;
+let isSunday = true;
+
+// Annotation : batana k konsa data type hai?
+let myName: string = "Imtiaz Ali";
+let roll: number = 1;
+let isWeakEnd: boolean = false;
