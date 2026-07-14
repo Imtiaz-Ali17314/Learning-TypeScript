b8db45d359ce7813d05025098cbd2edd9afa55a8
Imtiaz Ali <imtiazali17314@gmail.com>
2026-07-14 18:34:04 +0500

    feat: add examples for any and unknown types in TypeScript

---
+++ src/6.anyAndUnknown.ts
@@ -0,0 +1,27 @@
+// any type allows you to assign any type of value to a variable without any type checking.
+// However, it can lead to runtime errors if you try to call methods or access properties that do not exist on the assigned value.
+
+let value: any;
+
+value = "Imtiaz Ali";
+value = 22;
+value = true;
+value = { name: "Hussain Waris e Anbiya" };
+value = [1, 2, 3, 4, 5];
+
+value.toUpperCase();
+
+// unknown type is a safer alternative to any type. It allows you to assign any type of value to a variable,
+// but it requires you to perform type checking before calling methods or accessing properties on the assigned value.
+
+let newValue: unknown;
+
+newValue = "Imtiaz Ali";
+newValue = 22;
+newValue = true;
+newValue = { name: "Hussain Waris e Anbiya" };
+newValue = [1, 2, 3, 4, 5];
+
+if (typeof newValue === "string") {
+  newValue.toUpperCase();
+}
